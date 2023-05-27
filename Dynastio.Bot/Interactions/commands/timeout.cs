f1d4ff7f58d9cadce449fa7b88e4750cc4b0c3df
Jalal Jaleh <jalaljaleh@gmail.com>
2023-05-27 18:18:52 +0330

    better ui

--- Dynastio.Bot/Interactions/commands/timeout.cs
+++ Dynastio.Bot/Interactions/commands/timeout.cs
@@ -18,7 +18,7 @@ namespace Dynastio.Bot.Interactions.Modules
     public class timeoutModule : CustomInteractionModuleBase
     {
         [SlashCommand("mute", "mute a user")]
-        public async Task mute(IGuildUser user, TimeType time, int value,string reason = "no reason provided")
+        public async Task mute(IGuildUser user, TimeType time, int value, string reason = "no reason provided")
         {
             await DeferAsync();
             var time_ = value * (int)time;
@@ -29,7 +29,35 @@ namespace Dynastio.Bot.Interactions.Modules
             }
             var timeSpan = TimeSpan.FromSeconds(time_);
             await user.SetTimeOutAsync(timeSpan);
-            await FollowupAsync(embed: $"User <@{user.Id}> muted until {(DateTime.UtcNow + timeSpan).ToDiscordUnixTimestampFormat()} by {Context.User.Id.ToUserMention()} for ` {reason} `.".ToEmbed(user.Username + " Muted"));
+            await FollowupAsync(
+                text: user.Mention,
+                embed: new EmbedBuilder()
+                {
+                    Description = $"You have been muted by {Context.User.Id.ToUserMention()}.",
+                    ThumbnailUrl = user.GetAvatarUrl() ?? user.GetDefaultAvatarUrl(),
+                    Color = Color.DarkRed,
+                    Fields = new List<EmbedFieldBuilder>()
+                    {
+                        new EmbedFieldBuilder()
+                        {
+                            Name = "Duration",
+                            Value = value + " " + time.ToString(),
+                            IsInline = true
+                        },
+                         new EmbedFieldBuilder()
+                        {
+                            Name = "Revoke",
+                            Value =  (DateTime.UtcNow + timeSpan).ToDiscordUnixTimestampFormat(),
+                            IsInline = true
+                        },
+                          new EmbedFieldBuilder()
+                        {
+                            Name = "Moderator",
+                            Value = userMention,
+                            IsInline = true
+                        }
+                    }
+                }.Build());
         }
 
     }
