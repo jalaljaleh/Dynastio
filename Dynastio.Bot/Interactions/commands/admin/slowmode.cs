63189c9746950a37d0c5baf1d4dc088d80df85a1
Jalal Jaleh <jalaljaleh@gmail.com>
2023-06-05 17:53:51 +0330

    ..

---
+++ Dynastio.Bot/Interactions/commands/admin/slowmode.cs
@@ -0,0 +1,39 @@
+﻿using Discord.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Dynastio.Bot.Global;
+
+namespace Dynastio.Bot.Interactions.Modules
+{
+    [EnabledInDm(false)]
+    [RequireContext(ContextType.Guild)]
+    [RequireBotPermission(ChannelPermission.ManageChannels)]
+    [RequireUserPermission(GuildPermission.ManageChannels)]
+    [DefaultMemberPermissions(GuildPermission.ManageChannels)]
+    public class slowmodeModule : CustomInteractionModuleBase
+    {
+
+        [SlashCommand("channel-slowmode-set", "Set Slowmode")]
+        public async Task slowmode(ITextChannel channel, TimeType time, int value)
+        {
+            await DeferAsync();
+            var slowModeInterval = value * (int)time;
+            if (slowModeInterval <= 21600) // api limit
+            {
+                await channel.ModifyAsync(a =>
+                {
+                    a.SlowModeInterval = slowModeInterval;
+                });
+                await FollowupAsync(embed: $"Slowmode is ready for this channel <#{channel.Id}>".ToEmbed());
+                return;
+            }
+            await FollowupAsync(embed: "Can not set more than 6 hours.".ToEmbed());
+        }
+
+    }
+
+}
---
+++ Dynastio.Bot/Interactions/commands/moderators/clear.cs
@@ -0,0 +1,40 @@
+﻿using Discord.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Dynastio.Bot.Global;
+using Dynastio.Net;
+
+namespace Dynastio.Bot.Interactions.Modules
+{
+    [EnabledInDm(false)]
+    [RequireContext(ContextType.Guild)]
+    [RequireBotPermission(ChannelPermission.ManageMessages)]
+    [RequireUserPermission(ChannelPermission.ManageMessages)]
+    //[DefaultMemberPermissions(GuildPermission.ManageMessages)]
+    public class clearModule : CustomInteractionModuleBase
+    {
+        [SlashCommand("clear", "clear messages")]
+        public async Task clear(int count, Direction direction = Direction.Before, ulong fromMessageId = 0)
+        {
+            await DeferAsync();
+
+            var channel = Context.Channel as ITextChannel;
+
+            IEnumerable<IMessage> messages;
+
+            if (fromMessageId != 0)
+                messages = await channel.GetMessagesAsync(fromMessageId, direction, count).FlattenAsync();
+            else
+                messages = await channel.GetMessagesAsync(count).FlattenAsync();
+
+            await channel.DeleteMessagesAsync(messages);
+
+            await FollowupAsync($"done, {count} messages deleted.");
+        }
+    }
+
+}
